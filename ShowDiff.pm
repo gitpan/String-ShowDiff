4cb6483b5eabc9f0b42e50769038b084e4de41b1
Janek Schleicher <bigj@kamelfreund.de>
2003-04-26 17:25:45 +0000

    Import of BIGJ/String-ShowDiff-0.02 from CPAN.
    gitpan-cpan-distribution: String-ShowDiff
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BIGJ/String-ShowDiff-0.02.tar.gz
    gitpan-cpan-author:       BIGJ
    gitpan-cpan-maturity:     released

--- ShowDiff.pm
+++ ShowDiff.pm
@@ -18,45 +18,71 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
-use Algorithm::Diff qw/sdiff/;
+use Algorithm::Diff;
 use Term::ANSIColor qw/colored/;
 
 sub ansi_colored_diff {
     my ($string, $changed_string, $options) = @_;
     $options ||= {};
     my %colors = (
-	'-' => 'on_red',
-	'+' => 'on_green',
-	'u' => 'reset',
-	%$options
-    );	
-    my @sdiff = sdiff(map {[split //, $_]} $string, $changed_string);
-    my $ansi = "";
-    my $pos = 0;
+        '-' => $options->{'-'} || 'on_red',
+        '+' => $options->{'+'} || 'on_green',
+        'u' => $options->{'u'} || 'reset',
+    );
+    my $context_re = $options->{context} || qr/.*/;
+    my $gap        = $options->{gap}     || '';
+    
+    my @sdiff = Algorithm::Diff::sdiff(map {[split //, $_]} $string, $changed_string);
+    my @ansi;
+    my $first_while_loop = 1;
     while (@sdiff and my ($mod, $s1, $s2) = @{shift @sdiff}) {
-	if ($mod =~ /[u+-]/) { 
-	    $ansi .= colored($s1 || $s2, $colors{$mod});
-	    next;	
-	} else {  # Must be a change
-	          # So take a look, whether there are more chars that could be
-		  # changed in a row
-	    while (@sdiff && $sdiff[0]->[0] eq 'c') {
-		$s1 .= $sdiff[0]->[1];          # if so, $s1 = chars to remove
-		                                # in a row
-		$s2 .= $sdiff[0]->[2];          # and $s2 to add
-		shift @sdiff;                   # This element was a change
-		                                # that is already in $s1, $s2
-						# and thus unnecessary now
-	    }
-	    $ansi .= join "", map({colored($_, $colors{'-'})} split //, $s1),
-		              map({colored($_, $colors{'+'})} split //, $s2);
-	}		     
+        if ($mod =~ /[+-]/) { 
+            push @ansi, colored($s1 || $s2, $colors{$mod});
+        } else {  # Must be either a change or a part of unchanged characters
+                  # So take a look, whether there are more chars that should be
+                  # handled in a row
+            while (@sdiff && $sdiff[0]->[0] eq $mod) {
+                $s1 .= $sdiff[0]->[1];          # if so, join all chars from the old
+                                                # string to $s1
+                $s2 .= $sdiff[0]->[2];          # and from the new to $s2
+                shift @sdiff;                   # The information of this element
+                                                # is already in $s1, $s2 and $mod
+                                                # and thus unnecessary now
+            }
+            if ($mod eq 'u') {
+                my $unchanged_part = _construct_glue(
+                    $s1, $context_re, $gap, $first_while_loop, @sdiff==0
+                );
+                push @ansi, _colorize_string($unchanged_part, $colors{'u'});
+            } else {
+                push @ansi,
+                    _colorize_string($s1, $colors{'-'}), 
+                    _colorize_string($s2, $colors{'+'}); 
+            }		     
+        }
+        $first_while_loop = 0;
     }
-    return $ansi;
+    return join "", @ansi;
 }
 
+# call with _colorize_string($string, $color)
+sub _colorize_string { join "", map {colored($_,$_[1])} split //, $_[0] }
+
+sub _construct_glue {
+    my ($full_string, $context_re, $gap, $at_beginning, $at_end) = @_;
+    my ($start) = $full_string =~ /^($context_re)/;
+    my ($end)   = $full_string =~ /($context_re)$/;
+    $_ ||= "" for ($start, $end);
+
+    # Return now the shorter string of either a constructed context with a gap
+    # string or the normal string between the two differences                          
+    my $start_gap_end = $start . $gap . $end;
+    return length($start_gap_end) < length($full_string)
+        ? $start_gap_end
+        : $full_string;
+}
 
 1;
 __END__
@@ -73,8 +99,11 @@ String::ShowDiff - Perl extension to help visualize differences between strings
   # or a bit more detailed:
   my %options = ('u' => 'reset',
                  '+' => 'on_green',
-		 '-' => 'on_red');
+        		 '-' => 'on_red');
   print ansi_colored_diff($oldstring, $newstring, \%options);
+
+  # or let's see only the changed words 
+  print ansi_colored_diff($old, $new, {context => qr/\w*/, gap => ' '});
 
 =head1 DESCRIPTION
 
@@ -97,22 +126,53 @@ Removed characters from C<$string> are shown by default with a red background,
 while added characters to C<$changed_string> are shown by default with a green background
 (the unchanged characters are shown with the default values for the terminal).
 
-The C<$options_hash> allows you to set the colors for the output.
+The C<$options_hash> allows you to set the colors for the output and
+the context to be shown.
 The variable is a reference to a hash 
-with three optional keys ('u' for the color of the unchanged parts,
-'-' for the removed parts and '+' for the added ones).
+with the optional keys: 'u' for the color of the unchanged parts,
+'-', '+' for the color of the removed and the added parts,
+'context' for a regexp specifying the context that shall be shown
+before and after a changed part and
+'gap' for the string that shall be shown between the contexts of two changings.
 The default values for the options are:
 
     my $default_options = {
-	'u' => 'reset',
-	'-' => 'on_red',
-	'+' => 'on_green'
+	'u'       => 'reset',
+	'-'       => 'on_red',
+	'+'       => 'on_green',
+    'context' => qr/.*/,
+    'gap'     => '',
     };
 
 The specified colors must follow the conventions for the
 C<colored> method of L<Term::ANSIColor>.
 Please read its documentation for details.
 
+The specified context must be a valid regexp, constructed with the 
+C<qr/.../> operator. Internal the context around a changing is created with 
+matching the preceding substring with C</($context_re)$> and the succeeding
+substring with C<^($context_re)>. That is important to know if you want to work
+with backreferences. As an additional group encloses your regexp pattern, the
+first of your own defined subgroup is in C<$2> instead of C<$1>. (That's not
+very nice, but still better than paying the price of using C<$&>).
+
+The C<gap> parameter describes how to fill the gap between two shown changings
+in their context. Here are some examples of these parameters:
+
+    print ansi_colored_diff($s1, $s2, {context => qr/.*/, gap => ''}); # default
+    # will print the complete combined string with the marked removings and
+    # additions
+
+    print ansi_colored_diff($s1, $s2, {context => qr/.{0,3}/, gap => ' ... '});
+    # will print all changings with a context of the left and right 3 chars
+    # and will join each of them with a space, 3 dots and a space
+    # Note that it is important to use qr/.{0,3}/ instead of qr/.../ to also
+    # show only a context of 0,1 or 2 chars at the beginning or end of the
+    # strings
+
+    print ansi_colored_diff($s1, $s2, {context => qr/\w*/, gap => ' '})
+    # will print all changed words and seperates them with a blank
+    
 =back
 
 =head2 EXPORT
---
+++ t/context.t
@@ -0,0 +1,47 @@
+use strict;
+use warnings;
+
+use String::ShowDiff qw/ansi_colored_diff/;
+use Term::ANSIColor qw/:constants uncolor colored/;
+
+use constant TEST_STRINGS => (
+    "Honsetie is for the most part less proftable than dishonesty.",
+    "Honesty is for the most part less profitable than dishonesty. -- Plato"
+);
+
+use constant TEST_OPTIONS => map {[$_->[0], [split //, $_->[1]], [split //, $_->[2]]]}
+(
+    [{context => qr/.*/, gap => ''},
+     "Honsestiye is for the most part less profitable than dishonesty. -- Plato",
+     "uuu-u+u-+-uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu+uuuuuuuuuuuuuuuuuuuuuu+++++++++"
+    ],
+    [{context => qr/.*/, gap => 'this should not be visible'},
+     "Honsestiye is for the most part less profitable than dishonesty. -- Plato",
+     "uuu-u+u-+-uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu+uuuuuuuuuuuuuuuuuuuuuu+++++++++"
+    ],
+    [{context => qr/.{0,3}/, gap => ' ... '},
+     "Honsestiye is ... rofitab ... ty. -- Plato",
+     "uuu-u+u-+-uuuuuuuuuuu+uuuuuuuuuuu+++++++++"
+    ],
+    [{context => qr/\w*/, gap => ' '},
+     "Honsestiye profitable  -- Plato",
+     "uuu-u+u-+-uuuuu+uuuuuu+++++++++"
+    ],
+);
+
+use constant COLORS => {
+    'u' => 'reset',
+    '+' => 'on_green',
+    '-' => 'on_red',
+};
+
+use Test::More tests => 4;
+
+foreach (TEST_OPTIONS) {
+    my ($options, $s12, $mod) = @$_;
+    is ansi_colored_diff(TEST_STRINGS,$options),
+       join("", map {colored($s12->[$_],COLORS->{$mod->[$_]})} (0 .. @{$mod}-1)),
+       "Checking options context=" . $options->{context} . " gap= " . $options->{gap};
+}
+
+print RESET;    
